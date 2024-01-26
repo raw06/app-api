9e0a039549386cbdc14e05844e82489f4d6c20a0
manhdat.tang <tangdatsdbg@gmail.com>
2024-01-26 15:10:59 +0700

    update file config

--- config/shopify.php
+++ config/shopify.php
@@ -7,5 +7,4 @@ return [
     'host' => env('APP_HOST'),
     "cdn_base_script_tag"        => env("CDN_BASE_SCRIPT_TAG", ""),
     "shared_secret" => env('SHOPIFY_API_SECRET', ""),
-    'scopes' => env('SHOPIFY_PERMISSIONS')
 ];
